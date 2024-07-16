c909c0236936837b38af26b5834902d1c70275dc
NhatCapital <voqnhat@gmail.com>
2024-07-17 01:13:26 +0800

    Tắt thông báo chào mừng khi truy cập

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-    alert("Chào mừng bạn đến với website chứng khoán của NhatCapital!");
+ //alert("Chào mừng bạn đến với website chứng khoán của NhatCapital!");
 });
