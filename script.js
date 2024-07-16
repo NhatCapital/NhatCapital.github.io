8edda5638ed538a558f00af04f8911b999850247
NhatCapital <voqnhat@gmail.com>
2024-07-17 00:51:16 +0800

    Thêm nội dung website

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Document is ready!');
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Chào mừng bạn đến với website chứng khoán của NhatCapital!");
 });
